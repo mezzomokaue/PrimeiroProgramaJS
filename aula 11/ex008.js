82fd1cbefa6dab6f1becf3ee1b04e9a22eea1ed3
Kauê Mezzomo <130859376+mezzomokaue@users.noreply.github.com>
2023-05-02 19:34:16 -0300

    HTML com IF e ELSE

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 110
+console.log(`A velocidade do seu carro é: ${vel}Km/H`)
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade permitida... Você foi MULTADO!')
+}
+console.log('Dirija usando cinto de segurança!')
