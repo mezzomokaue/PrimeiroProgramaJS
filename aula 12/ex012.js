f4f231cb4672654cbaebc527878595c8a34f8da0
Kauê Mezzomo <130859376+mezzomokaue@users.noreply.github.com>
2023-05-02 20:08:49 -0300

    Usando SWITCH

---
+++ aula 12/ex012.js
@@ -0,0 +1,10 @@
+var idade = 16
+if (idade > 18){
+    console.log('Você pode votar!')
+}else{
+    if(idade >= 16){
+        console.log('Voto opcional!')
+    }else{
+        console.log("Você não pode votar!")
+    }
+}
